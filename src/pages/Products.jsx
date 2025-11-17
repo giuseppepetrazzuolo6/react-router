f95c748a6743cd6ac16a072add3129c3345e3e89
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-17 12:16:49 +0100

    added content in Products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,42 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 export default function Products() {
-    const productsApiUrl = ''
+    const productsApiUrl = 'https://fakestoreapi.com/products'
+    const [product, setProduct] = useState([])
+
+    function getProducts() {
+        axios.get(productsApiUrl)
+            .then(response => {
+                const productsList = response.data
+                setProduct(productsList)
+            })
+            .catch(error => console.error(error))
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-md-6 col-lg-4">
-                    <h1>ciao</h1>
-                </div>
-            </div>
+        <div className="container p-4">
+            <h1>I nostri Articoli</h1>
+            <div className="row g-3">
+                {
+                    product.map(item =>
+                        < div className="col-12 col-md-6 col-lg-4" key={item.id}>
+                            <div className="card h-100">
+                                <div className="card-img-top">
+                                    <img className="product-img" src={item.image} alt="" />
+                                </div>
+                                <div className="card-body">
+                                    <h3>{item.title}</h3>
+                                    <span>{item.price}</span>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div >
         </div>
     )
 }

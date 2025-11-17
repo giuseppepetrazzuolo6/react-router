ef7e0d0af380483f1fa46f7c0aa3b52423e29006
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-17 11:22:47 +0100

    Create AboutUs content

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,27 @@
+import bancarellaImg from "../assets/img/polizia_locale_bancarella-2.jpg"
 export default function AboutUs() {
 
     return (
-        <h1>Chi Siamo</h1>
+        <div class="p-5 mb-4 bg-light ">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Chi siamo</h1>
+                <p class="col fs-4">
+                    Fondata oltre 20 anni fa,
+                    la nostra azienda nasce dalla passione per le bancarelle.
+                    Sin dai primi giorni, l’obiettivo è stato offrire ai clienti soluzioni affidabili e innovative,
+                    con un’attenzione particolare alla qualità e al servizio.
+                    Con il tempo, siamo cresciuti,
+                    ampliando il catalogo e abbracciando le nuove tecnologie,
+                    senza mai perdere di vista i valori che ci hanno guidato fin dall’inizio: integrità,
+                    passione e cura del cliente.
+                    Oggi siamo orgogliosi di continuare a portare sul mercato prodotti selezionati con cura e un’esperienza d’acquisto unica,
+                    mantenendo vivo lo spirito che ci ha contraddistinto fin dalla fondazione.
+                </p>
+            </div>
+            <div>
+                <img className="w-100" src={bancarellaImg} alt="" />
+            </div>
+        </div>
+
     )
 }

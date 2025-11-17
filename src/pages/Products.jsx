aa0788ba261dfd324384bbb31a4e47711c77b82c
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-17 11:38:31 +0100

    Create base structure for Products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,15 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
 export default function Products() {
+    const productsApiUrl = ''
 
     return (
-        <h1>Prodotti</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <h1>ciao</h1>
+                </div>
+            </div>
+        </div>
     )
 }

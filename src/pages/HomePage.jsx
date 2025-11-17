c1d13cec58b0804dca5ca2e42ca44a9df3c8561c
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-17 11:04:06 +0100

    Create HomePage content and add button to navigate to About page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/about");
+    };
 
     return (
-        <h1>Home</h1>
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">Benvenuto nel nostro Store Online!</h1>
+                <p className="col-md-8 fs-4">
+                    Siamo felici di averti qui. <br />
+                    Nel nostro shop troverai prodotti selezionati con cura,
+                    qualità garantita e un servizio dedicato a offrirti la migliore esperienza possibile. <br />
+                    Dai un’occhiata alle nostre categorie, scopri le novità e lasciati ispirare dalle nostre proposte.
+                    Se hai dubbi o domande, siamo sempre a tua disposizione!
+                </p>
+                <button
+                    className="btn btn-dark btn-lg"
+                    type="button"
+                    onClick={handleClick}>
+                    Scopri di più
+                </button>
+            </div>
+        </div>
+
     )
 }
